c86e150b4e530bee0771fcff68653ae137575038
oguama <oguama@bancolombia.com.co>
2021-08-19 21:05:54 -0500

    sumar()

--- src/main/java/com/practicagit/git1/Git1Application.java
+++ src/main/java/com/practicagit/git1/Git1Application.java
@@ -8,7 +8,7 @@ public class Git1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Git1Application.class, args);
-		System.out.println("hola");
+		//System.out.println("hola");
 
 	}
 
