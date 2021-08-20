3728a098074a8c1b2eb73ef94ea69c69d3548c01
oguama <oguama@bancolombia.com.co>
2021-08-19 19:40:18 -0500

    fabrica

---
+++ src/main/java/com/practicagit/git1/Git1Application.java
@@ -0,0 +1,15 @@
+package com.practicagit.git1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Git1Application.class, args);
+		System.out.println("hola");
+
+	}
+
+}
---
+++ src/main/java/com/practicagit/git1/controller/myController.java
@@ -0,0 +1,15 @@
+package com.practicagit.git1.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class myController {
+
+
+}
